cffccb8734e44558bd8009b20294e32a1bf982cb
bryhkim9494 <bryhkim9494@gmail.com>
2023-07-04 18:15:15 +0900

    리액트 todo CRUD

--- src/components/todo2/Todo2List.js
+++ src/components/todo2/Todo2List.js
@@ -4,7 +4,7 @@ import { getList } from "../../api/todoAPI";
 const initState = { dtoList: [] };
 
 
-const Todo2List = ({changeView}) => {
+const Todo2List = ({ changeView }) => {
 
     const [data, setData] = useState(initState)
     useEffect(() => {
@@ -17,11 +17,12 @@ const Todo2List = ({changeView}) => {
     return (
         <div className="w-full h-[100vh] bg-sky-500">
             <div className="text-3xl">Todo2 List
-            <button onClick={() => changeView('input')}>INPUT</button>
+                <button onClick={() => changeView('input')}>INPUT</button>
             </div>
 
             <ul>
-                {data.dtoList.map(todo => <li key={todo.tno}>{todo.tno} - {todo.title}</li>)}
+                {data.dtoList.map(todo =>
+                    <li key={todo.tno} onClick={() => changeView('read', todo.tno)} >{todo.tno} - {todo.title}</li>)}
 
             </ul>
         </div>
--- src/components/todo2/index.js
+++ src/components/todo2/index.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { getList } from "../../api/todoAPI";
 import Todo2List from "./Todo2List";
 import Todo2Input from "./Todo2Input";
+import Todo2Read from "./Todo2Read";
 
 
 const Todo2 = () => {
@@ -11,16 +12,25 @@ const Todo2 = () => {
 
     //라우팅 설계
     const [cmd, setCmd] = useState("list")
-    const changeView = (value) => {
+    const [target, setTarget] = useState(0);
+
+    const changeView = (value, tno) => {
         setCmd(value)
+
+        if (tno) {
+            setTarget(tno)
+        }
     }
+
     const getView = () => {
         if (cmd === 'list') {
             return <Todo2List changeView={changeView}></Todo2List>
         } else if (cmd === 'input') {
             return <Todo2Input changeView={changeView}></Todo2Input>
+        } else if( cmd === 'read'){
+            return <Todo2Read target={target} changeView={changeView}></Todo2Read>
         }
-    }
+    } 
 
 
 
@@ -28,7 +38,14 @@ const Todo2 = () => {
     return (
 
         <>
-            {getView()}
+            <div className="w-full h-24 bg-blue-500 flex justify-center">
+                <div className="border-2 text-red w-32" onClick={() => changeView("list")}>LIST</div>
+                <div className="border-2 text-red w-32" onClick={() => changeView("input")}>INPUT</div>
+            </div>
+            <div>
+                {getView()}
+
+            </div>
         </>
     );
 }
